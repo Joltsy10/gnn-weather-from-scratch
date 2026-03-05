fce015735ac24980fb5fec75f7a738995e5a8172
Rayyan Farooq <farooqrayyann@gmail.com>
2026-03-05 05:50:11 +0530

    added a baseline to compare against

---
+++ training/baseline.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+node_features = torch.tensor(
+        np.load('data/node_features.npy'), dtype=torch.float32
+    ) # (T, N, 7)
+
+T = node_features.shape[0]
+
+loss_fn = nn.MSELoss()
+total_loss = 0
+
+for t in range(T-1):
+    loss = loss_fn(node_features[t], node_features[t+1])
+
+    total_loss += loss
+
+avg_loss = total_loss/(T - 1)
+print(avg_loss)
