7e3493c6dc1ba68895aa7f0c880d01d96c558c37
Rayyan Farooq <farooqrayyann@gmail.com>
2026-03-12 06:12:27 +0530

    updated training to be able to handle global

--- training/train.py
+++ training/train.py
@@ -6,6 +6,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from model.gnn import GNN
+from model.hi_gnn import HiGNN
 
 def load_config(path='config.yaml'):
     with open(path) as f:
@@ -17,24 +18,51 @@ def load_data(data_dir):
     edge_features = torch.load(f'{data_dir}/edge_features.pt')  # (E, edge_dim)
     return node_features, edge_index, edge_features
 
+def load_global_graph(graph_dir, device):
+    num_levels = torch.load(f'{graph_dir}/m2m_edge_index.pt', 
+                            map_location=device)
+    # m2m_edge_index.pt is a list, one per level
+    m2m_edge_index = torch.load(f'{graph_dir}/m2m_edge_index.pt', map_location=device)
+    num_levels = len(m2m_edge_index)
+
+    graph = {
+        'g2m_edge_index': torch.load(f'{graph_dir}/g2m_edge_index.pt', map_location=device),
+        'g2m_features':   torch.load(f'{graph_dir}/g2m_features.pt',   map_location=device),
+        'm2g_edge_index': torch.load(f'{graph_dir}/m2g_edge_index.pt', map_location=device),
+        'm2g_features':   torch.load(f'{graph_dir}/m2g_features.pt',   map_location=device),
+        'm2m_features':   torch.load(f'{graph_dir}/m2m_features.pt',   map_location=device),
+        'up_edge_index':  torch.load(f'{graph_dir}/mesh_up_edge_index.pt',   map_location=device),
+        'up_features':    torch.load(f'{graph_dir}/mesh_up_features.pt',     map_location=device),
+        'down_edge_index':torch.load(f'{graph_dir}/mesh_down_edge_index.pt', map_location=device),
+        'down_features':  torch.load(f'{graph_dir}/mesh_down_features.pt',   map_location=device),
+        'mesh_features':  torch.load(f'{graph_dir}/mesh_features.pt',        map_location=device),
+    }
+    return graph, num_levels
+
 def train(device='cpu', resume=False):
     config  = load_config()
     domain  = config['domain']
     data_dir = f'data/{domain}'
+    node_dim = config['model']['node_dim']
 
-    node_features, edge_index, edge_features = load_data(data_dir)
-
-    edge_index    = edge_index.to(device)
-    edge_features = edge_features.to(device)
-    node_features = node_features.to(device)
+    if domain == 'lam':
+        node_features, edge_index, edge_features = load_data(data_dir)
+        edge_index    = edge_index.to(device)
+        edge_features = edge_features.to(device)
+        node_features = node_features.to(device)
+        edge_dim = edge_features.shape[1]
+        model = GNN(node_dim=node_dim, edge_dim=edge_dim).to(device)
+    else:
+        node_features = torch.load(f'{data_dir}/node_features.pt')
+        node_features = node_features.to(device)
+        graph, num_levels = load_global_graph(data_dir, device)
+        edge_dim = graph['g2m_features'].shape[1]
+        model = HiGNN(node_dim=node_dim, edge_dim=edge_dim, 
+                    num_levels=num_levels).to(device)
 
     train_data = node_features[:2688].to(device)
     val_data   = node_features[2688:4032].to(device)
 
-    node_dim = config['model']['node_dim']
-    edge_dim = edge_features.shape[1]
-
-    model = GNN(node_dim=node_dim, edge_dim=edge_dim).to(device)
     if resume and os.path.exists(f'{data_dir}/model.pt'):
         model.load_state_dict(torch.load(f'{data_dir}/model.pt', map_location=device))
         print("Resumed from checkpoint")
@@ -53,7 +81,10 @@ def train(device='cpu', resume=False):
             x    = train_data[t]
             loss = 0
             for k in range(K):
-                delta = model(x, edge_index, edge_features)
+                if domain == 'lam':
+                    delta = model(x, edge_index, edge_features)
+                else:
+                    delta = model(x, graph)
                 pred  = x + delta
                 loss += loss_fn(pred, train_data[t + k + 1])
                 x = pred
@@ -70,7 +101,10 @@ def train(device='cpu', resume=False):
             for t in range(len(val_data) - K):
                 x = val_data[t]
                 for k in range(K):
-                    delta = model(x, edge_index, edge_features)
+                    if domain == 'lam':
+                        delta = model(x, edge_index, edge_features)
+                    else:
+                        delta = model(x, graph)
                     pred  = x + delta
                     val_total += loss_fn(pred, val_data[t + k + 1]).item()
                     x = pred
